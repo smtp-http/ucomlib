91dc3aaa38cb87e26cba82c573f7f4d25bd8cbc3
zukeqiang <zukeqiang@voyagerauto.com>
2020-04-09 10:20:25 +0000

    test

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 //#include "Timer.h"
 #include <stdio.h>
 //#include "list.h"
+#define __DEBUG__
 #include "ucomlib.h"
+#include <pthread.h>
+#include <string.h>
 
+pthread_mutex_t g_mutex;
 
 int handler(const int event,const int msgId)
 {
@@ -19,23 +23,59 @@ int handler(const int event,const int msgId)
 
 void ENTER_CRITICAL()
 {
-
+	pthread_mutex_lock(&g_mutex);
 }
 
 
 void EXIT_CRITICAL()
 {
-
+	pthread_mutex_unlock(&g_mutex);
 }
 
 int CRITICAL_INIT()
 {
-
+	pthread_mutex_init(&g_mutex, NULL);
 }
 
 int CRITICAL_DEINIT()
 {
+	pthread_mutex_destroy(&g_mutex);
+}
 
+
+void *thread_timer(void* arg)
+{
+	printf("new thread,thread is :%u,pid:%d\n",pthread_self(),getpid());
+	while(1) {
+		sleep(1);
+		OnTimer();
+		//pthread_exit(NULL);
+	}
+}
+
+char *msg1 = "hello hehe hehe hhe hs";
+void *thread_run1(void* arg)
+{
+	printf("new thread,thread is :%u,pid:%d\n",pthread_self(),getpid());
+	CommSaveMsg(11,msg1,strlen(msg1));
+	while(1) {
+		sleep(5);
+		printf("save ret:%d\n",CommSaveMsg(10,msg1,strlen(msg1)));
+	}
+}
+
+void *thread_run2(void* arg)
+{
+	char ms[100];
+	int ret,len;
+	printf("new thread,thread is :%u,pid:%d\n",pthread_self(),getpid());
+	while(1) {
+		sleep(3);
+
+		ret = CommGetMsg(10,ms,&len);
+		
+		printf("get ret:%d  len:%d\n",ret,len);
+	}
 }
 
 
@@ -46,5 +86,15 @@ int main()
 {
 	CommuInit();
 	CommSetEventHandler(handler);
-	printf("hello");
+
+	pthread_t tid_timer,tid_run1,tid_run2;
+	pthread_create(&tid_timer,NULL,thread_timer,NULL);
+	pthread_create(&tid_run1,NULL,thread_run1,NULL);
+	pthread_create(&tid_run2,NULL,thread_run2,NULL);
+
+	while(1){
+		sleep(10);
+	}
+
+	return 0;
 }
